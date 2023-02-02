09c8cae6a88285d2f1073e5c84df5c62bd5af4f8
matthiasmulzet <mulzetmatthias@gmail.com>
2023-02-02 21:02:42 +0100

    installed firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBRdsADcPbPhGP7FnGXsiFLp_K6b_sCMWE",
+        authDomain: "slackclone-2dd75.firebaseapp.com",
+        projectId: "slackclone-2dd75",
+        storageBucket: "slackclone-2dd75.appspot.com",
+        messagingSenderId: "452854262762",
+        appId: "1:452854262762:web:a54d2e759b33fd5ec02093"
+    }
+};
+
