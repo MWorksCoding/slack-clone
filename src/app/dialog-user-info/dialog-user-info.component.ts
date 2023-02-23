1377ee72656d53ed032201b6e8ebc890f98e2556
matthiasmulzet <mulzetmatthias@gmail.com>
2023-02-23 19:03:57 +0100

    implement upload image

--- src/app/dialog-user-info/dialog-user-info.component.ts
+++ src/app/dialog-user-info/dialog-user-info.component.ts
@@ -18,17 +18,19 @@ export class DialogUserInfoComponent {
   selectedImage: any;
   uploadPercent: Observable<number> | undefined;
   downloadURL: Observable<string> | undefined;
-  updatedEmail: string = '';
-  updatedUserName: string = '';
   emailForm = new FormControl(this.auth.currentEmail, [Validators.required, Validators.email]);
-  usernameForm = new FormControl('', [Validators.required, Validators.minLength(2)]);
+  usernameForm = new FormControl(this.auth.currentUserName, [Validators.required, Validators.minLength(2)]);
 
 
   constructor(public dialog: MatDialog, public auth: AuthService, public firestore: AngularFirestore, public storage: AngularFireStorage,
     public spinnerService: SpinnerService) {
-    // this.updatedEmail = this.auth.currentEmail;
-    // this.emailForm.setValue(this.auth.currentEmail);
-    this.updatedUserName = this.auth.currentUserName;
+  }
+
+
+  getErrorMessageUsername() {
+    if (this.usernameForm.hasError('required'))
+      return 'You must enter a Username'
+    return this.usernameForm.hasError('minlength') ? 'Not a valid username' : '';
   }
 
 
@@ -38,21 +40,23 @@ export class DialogUserInfoComponent {
     return this.emailForm.hasError('email') ? 'Not a valid email' : '';
   }
 
+  
   updateUserInfos() {
-    this.auth.updateEmailAndName(this.emailForm.value, this.updatedUserName);
+    this.auth.updateEmailAndName(this.emailForm.value, this.usernameForm.value);
   }
 
 
-  onFileSelected(event: any): void {
+  onFileSelected(event: any, userId: string): void {
     this.selectedImage = event.target.files[0];
+    this.uploadImage(userId);
   }
 
 
-  uploadImage() {
+  uploadImage(userId: string) {
     if (!this.selectedImage) {
       return;
     }
-    const filePath = `profile-pictures/${Date.now()}_${this.selectedImage.name}`;
+    const filePath = `users/${userId}/profile-pictures/${Date.now()}_${this.selectedImage.name}`;
     const fileRef = this.storage.ref(filePath);
     const task = this.storage.upload(filePath, this.selectedImage);
 
