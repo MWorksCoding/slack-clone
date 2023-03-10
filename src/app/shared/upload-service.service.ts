e8a447edf40fe7f811d7e925de26d2b65ab7985b
matthiasmulzet <mulzetmatthias@gmail.com>
2023-03-10 13:21:11 +0100

    worked on signup error storage does not exist

--- src/app/shared/upload-service.service.ts
+++ src/app/shared/upload-service.service.ts
@@ -1,55 +1,51 @@
 import { Injectable } from '@angular/core';
-import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
+import {
+  AngularFireStorage,
+  AngularFireUploadTask,
+  BUCKET,
+} from '@angular/fire/compat/storage';
 import { UploadTaskSnapshot } from '@angular/fire/compat/storage/interfaces';
-import { BehaviorSubject, lastValueFrom } from 'rxjs';
+import { FieldPath } from 'firebase/firestore';
+import { BehaviorSubject, finalize, lastValueFrom, Subscription } from 'rxjs';
 import { __await } from 'tslib';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UploadServiceService {
 
 
-  constructor(public storage: AngularFireStorage) { }
+  constructor(public storage: AngularFireStorage) {}
 
 
   public url$ = new BehaviorSubject<string>('assets/img/name-icon.png');
 
 
-    /**
+  /**
    * loads default image to storage
-   * @param userId {string} 
+   * @param userId {string}
    */
-    async uploadNewImage(userId: string | undefined) {
-      const filePath = `users/${userId}/profile-picture`;
-      const storageFile = this.storage.ref(filePath);
-      const imageUrl = "assets/img/name-icon.png";
-      const response = await fetch(imageUrl);
-      const file = await response.blob();
-      let uploadTask = storageFile.put(file);
-      this.updateProfilePicture(uploadTask);
-    }
-  
-  
-    /**
-     * get url from uploadtask and update url$ with it, profile picture will be updated
-     * @param uploadTask {AngularFireUploadTask} represents current upload task for the file
-     * that is being uploaded to the storage
-     */
-    updateProfilePicture(uploadTask: AngularFireUploadTask) {
-      uploadTask.then((snapshot) => {
-        snapshot.ref.getDownloadURL()
-          .then((downloadURL: string) => {
-            this.url$?.next(downloadURL);
-            console.log('url§', downloadURL);
-          })
-      });
-    }
+  async uploadNewImage(userId: string | undefined) {
+    const filePath = `users/${userId}/profile-picture`;
+    const fileRef = this.storage.ref(filePath);
+    fileRef.getDownloadURL().subscribe(
+      url => {
+        console.log(`File ${filePath} exists at ${url}`);
+      }
+    );
+
+
+    const imageUrl = 'assets/img/name-icon.png';
+    const response = await fetch(imageUrl);
+    const file = await response.blob();
+    const task = await this.storage.upload(filePath, file);
+    this.getUrl(task);
+  }
 
 
   /**
    * selected file will be passed to uploadImage function
-   * @param event {Event} represents a change event 
+   * @param event {Event} represents a change event
    * @param userId {string}
    */
   onFileSelected(event: any, userId: string): void {
@@ -60,23 +56,22 @@ export class UploadServiceService {
 
   /**
    * delete existing profile picture from storage and uploads a new profil picture
-   * @param userId {string} 
-   * @param selectedImage {File} 
+   * @param userId {string}
+   * @param selectedImage {File}
    */
   async uploadImage(userId: string | undefined, selectedImage: File) {
     const filePath = `users/${userId}/profile-picture`;
-    const storageFile = this.storage.ref(filePath);
-    if (storageFile)
-      storageFile.delete()
+    const fileRef = this.storage.ref(filePath);
+    fileRef.delete();
     const task = await this.storage.upload(filePath, selectedImage);
     this.getUrl(task);
   }
 
-  
-/**
- * updates url$ with the actual profil picture from storage
- * @param task {UploadTaskSnapshot}
- */
+
+  /**
+   * updates url$ with the actual profil picture from storage
+   * @param task {UploadTaskSnapshot}
+   */
   async getUrl(task: UploadTaskSnapshot) {
     const url = await task.ref.getDownloadURL();
     this.url$.next(url);
